060bd5679315f435506e0c838bba15437697c646
priyankaraut10 <priyankaraut10@gmail.com>
2024-03-12 08:42:06 +0530

    added 3 new ml model codes

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("addding all ml related code here")
+
+
+
+def model1():
+    print("adding code for ML model1")
+
+def model2():
+    print("adding code for ML model2")
+
+def model3():
+    print("adding code for ML model3")
